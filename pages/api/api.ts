e24e64c41ed005f9ce47dc686ddf45a4578c9821
Divyanshu Yadav <divyanshu.1810@gmaiil.com>
2023-01-19 00:10:09 +0530

    :tada: Project Started

---
+++ pages/api/api.ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios';
+const newsApi = process.env.NEWS_API;
+type Data = {
+  name: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const data = await axios.get(
+      `${newsApi}&category=${req.query.category}`
+    );
+    res.json({ name: data.data.articles });
+  } catch (err) {
+    console.log(err);
+    // res.sendStatus(500);
+  }
+}
